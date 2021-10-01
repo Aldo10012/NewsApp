dce92ac68e44a89bf5655ef340e3522934458d40
Alberto <aldo10012@gmail.com>
2021-10-01 11:43:40 -0700

    init project

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Alberto Dominguez on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
